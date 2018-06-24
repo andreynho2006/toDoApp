761c39dcdc6bef902f46dcdd4b14db16ac5bfe07
andrei cirlan <andreynho2006@gmail.com>
2018-06-24 19:22:59 +0300

    make a class completed to handle the behavior of the list item on click

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,4 @@
 //check off specific todos by clicking\
 $("li").click(function() {
-    $(this).css("color","gray");
-    $(this).css("text-decoration","line-through");
+    $(this).toggleClass("completed");
 });
