e2fd1043b6ab5bd20c816cee3f411319dc6640c0
andreynho2006 <andreynho2006@gmail.com>
2018-06-25 07:28:23 +0300

    style the container , add plus icon and trash icon for adding and delete items and style them

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,23 @@
 //check off specific todos by clicking\
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 //click on span to delete todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
-})
+});
+
+//add toDo items
+$("input[type='text'").keypress(function(event) {
+    if(event.which === 13) {
+        //grab todotext from input
+        var toDoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash-alt'></i></span>" + toDoText + "</li>");
+    }
+});
